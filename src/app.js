b9859b3c83ed88400e7128d160500ac1bda6c45d
VjacheslavZ <slavikov.net@mail.ru>
2018-07-02 00:31:31 +0300

    end

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@
 
 
 import  {GitHub}  from "./github";
+import { UI } from './ui'
 
 //init
 const github = new GitHub();
+const ui = new UI();
 
 const searchUser = document.getElementById('searchUser');
 
@@ -12,6 +14,16 @@ searchUser.addEventListener('keyup', (e) => {
    const userText = e.target.value;
 
    if(userText !== '') {
-       github.getUser(userText).then(data => {console.log(data)})
+       github.getUser(userText)
+           .then(data => {
+               if(data.profile.message === 'Not Found'){
+                   ui.showAlert('User not found', 'alert alert-danger');
+               } else {
+                   ui.showProfile(data.profile);
+                   ui.showRepos(data.repos);
+               }
+           })
+   } else {
+        ui.clearProfile();
    }
 });
